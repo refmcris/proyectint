5e4a5eaa4f9318e603cf7e0a2a734e7492b06964
refmcris <cristianmartineztez@gmail.com>
2024-10-24 15:36:03 -0500

    Agrego descripcion y imagen para los equipos

--- src/Components/Dashboard/equipos.jsx
+++ src/Components/Dashboard/equipos.jsx
@@ -1,13 +1,19 @@
   import React, { useState, useEffect } from "react";
   import axios from "axios";
-  import {Button, Box, Dialog, DialogTitle, DialogContent, DialogActions,TextField, FormControl, InputLabel, Select, MenuItem, Typography, Table, TableContainer, TableHead, TableRow, TableCell, TableBody, Paper, TablePagination, TableSortLabel, Grid} from "@mui/material";
+  import {Button, Box, Dialog, DialogTitle,CircularProgress, DialogContent, DialogActions,TextField, FormControl, InputLabel, Select, MenuItem, Typography, Table, TableContainer, TableHead, TableRow, TableCell, TableBody, Paper, TablePagination, TableSortLabel, Grid, Tooltip, IconButton, styled} from "@mui/material";
   import SideBar from "./sidebar";
-  import Draggable from "react-draggable";
   import { exportExcel } from "../../Common/exportExcel";
+  import InsertDriveFileIcon from '@mui/icons-material/InsertDriveFile';
+  import { ToastContainer, toast } from 'react-toastify'; 
+  import 'react-toastify/dist/ReactToastify.css';
+
+
+  import { Cloudinary } from 'cloudinary-core';
+
+
   const initialData = [];
 
   const columns = [
-    { id: "id_equipo", label: "ID Equipo" },
     { id: "nombre_equipo", label: "Nombre Equipo" },
     { id: "serial", label: "Serial" },
     { id: "tipo", label: "Tipo" },
@@ -16,6 +22,9 @@
     { id: "estado", label: "Estado" },
     { id: "ubicación", label: "Ubicación" },
     { id: "fecha_ingreso", label: "Fecha de Ingreso" },
+    { id: "descripcion", label: "Descripcion" },
+    { id: "imagen", label: "Imagen" },
+
   ];
 
   function Equipos() {
@@ -29,8 +38,13 @@
     const [page, setPage] = useState(0);
     const [rowsPerPage, setRowsPerPage] = useState(10);
     const [searchTerm, setSearchTerm] = useState('');
+    const [imageBase64, setImageBase64] = useState('');
+    const [loading, setLoading] = useState(false);
+    const [imageUploaded, setImageUploaded] = useState(false);
 
-    useEffect(() => {
+
+    const cl = new Cloudinary({ cloud_name: 'tu_cloud_name' });
+    
       const fetchData = async () => {
         try {
           const response = await axios.get("http://localhost:3001/api/equipos");
@@ -42,17 +56,20 @@
       };
 
       fetchData();
-    }, []);
+    
 
     const handleOpenDialog = () => {
       setOpenDialog(true);
       setEditMode(false);
       setEditRecord({});
+      setImageBase64('');
+      setImageUploaded(false);
     };
   
     const handleCloseDialog = () => {
       setOpenDialog(false);
       setEditRecord({});
+      setImageBase64('');
     };
 
     const handleInputChange = (e) => {
@@ -104,6 +121,36 @@
         },
       });
     };
+  const handleFileChange = async (event) => {
+    const file = event.target.files[0];
+    const MAX_SIZE = 5 * 1024 * 1024; 
+
+    if (file && file.size > MAX_SIZE) {
+      alert("El archivo es demasiado grande. El límite es de 5MB.");
+      return;
+    }
+    const allowedTypes = ['image/jpeg', 'image/png', 'image/gif'];
+    if (file && !allowedTypes.includes(file.type)) {
+      alert("Por favor, sube un archivo de imagen (JPG, PNG, GIF).");
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("upload_preset", "Uninventorys"); 
+    setLoading(true); 
+
+    try {
+      const response = await axios.post(`https://api.cloudinary.com/v1_1/ds6fxjeak/image/upload`, formData);
+      const imageUrl = response.data.secure_url;
+      setImageBase64(imageUrl); 
+      setImageUploaded(true); 
+    } catch (error) {
+      console.error("Error al subir la imagen a Cloudinary:", error);
+    } finally {
+      setLoading(false); 
+    }
+  };
 
     const sortedData = filteredData
       .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
@@ -136,27 +183,31 @@
       };
       const handleSubmit = async (event) => {
         event.preventDefault();
-        const postData = { ...editRecord };
-        const url = "http://localhost:3001/api/registro";
-    
+        const postData = {
+          ...editRecord,
+          imagen: imageBase64 || editRecord.imagen 
+        };
+        const url = "http://localhost:3001/api/registro";    
         try {
           if (editMode) {
             await axios.put(`${url}/${editRecord.id_equipo}`, postData);
-            alert("Registro actualizado con éxito");
+            toast.success("Registro actualizado con éxito!",{autoClose:2000});
             setData((prevData) =>
-              prevData.map((item) =>
-                item.id_equipo === editRecord.id_equipo ? postData : item
-              )
+                prevData.map((item) =>
+                    item.id_equipo === editRecord.id_equipo ? postData : item
+                )
             );
+            fetchData();
+            
           } else {
             await axios.post(url, postData);
-            alert("Registro agregado con éxito");
+            toast.success("Registro agregado con éxito!",{autoClose:2000});
             setData((prevData) => [...prevData, postData]);
+            fetchData();
           }
           handleCloseDialog();
         } catch (error) {
-          console.error("Error al guardar registro", error);
-          alert("Error al guardar registro");
+          toast.error("Error al guardar registro!",{autoClose:2000});
         }
       };
       const handleSearchChange = (event) => {
@@ -173,10 +224,15 @@
         setFilteredData(filtered);
         setPage(0);
       };
+
+      
     return (
       <Box sx={{ display: "flex" }}>
         <SideBar />
         <Box component="main" sx={{ flexGrow: 1, p: 3, marginTop: "55px" }}>
+        <Typography variant="h4" sx={{ marginBottom: 2, color: '#333' }}>
+          Registro de equipos
+        </Typography>
           <Box sx={{display: "flex",justifyContent: "space-between",alignItems: "center", mb: 2,}}><TextField
           label="Buscar equipos"
           variant="outlined"
@@ -187,9 +243,14 @@
             <Button variant="contained" onClick={handleOpenDialog} sx={{ backgroundColor: "#d01c35" }}>
               Agregar Equipo
             </Button>
-            <Button variant="contained" onClick={handleExport}>
-            Exportar a Excel
-          </Button>
+          </Box>
+          <Box sx ={{display:"flex",justifyContent:"flex-end"}}>
+            <Tooltip title="Exportar a excel">
+                <IconButton onClick={handleExport}   sx={{backgroundColor: '#2e7d32','&:hover': {backgroundColor: '#1b5e20',}}}>
+                  <InsertDriveFileIcon sx={{ color: '#eef5f1' }}/>
+                </IconButton>
+            </Tooltip>
+
           </Box>
           
           <TableContainer component={Paper} sx={{borderRadius: '10px', boxShadow: '0px 4px 20px rgba(0, 0, 0, 0.1)'}}>
@@ -208,29 +269,56 @@
                 </TableRow>
               </TableHead>
               <TableBody>
-                {sortedData.map((row) => (
-                  <TableRow key={row.id_equipo}>
-                    {columns.map((column) => (
-                      <TableCell key={column.id}>
+              {sortedData.map((row) => (
+                <TableRow key={row.id_equipo}>
+                  {columns.map((column) => (
+                    <TableCell key={column.id}>
                       {column.id === "estado" ? (
                         <span
-                          style={{  backgroundColor: getEstadoColor(row[column.id]),  padding: "5px 10px",   borderRadius: "5px",    display: "inline-block" }}
+                          style={{
+                            backgroundColor: getEstadoColor(row[column.id]),
+                            padding: "5px 10px",
+                            borderRadius: "5px",
+                            display: "inline-block"
+                          }}
                         >
                           {row[column.id]}
                         </span>
+                      ) : column.id === "imagen" ? (
+                        <>
+                          {row[column.id] ? (
+                            <img 
+                              src={row[column.id]}
+                              alt="Imagen del equipo" 
+                              style={{ width: '50px', height: '50px', objectFit: 'cover' }} 
+                              onClick={() => window.open(row[column.id])}
+                            />
+                          ) : (
+                            <Typography variant="caption" color="textSecondary">Sin imagen</Typography>
+                          )}
+                        </>
+                      ) : column.id === "fecha_ingreso" ? (
+                        new Date(row.fecha_ingreso).toLocaleString('es-ES', {
+                          year: 'numeric',
+                          month: '2-digit',
+                          day: '2-digit',
+                          hour: '2-digit',
+                          minute: '2-digit',
+                          second: '2-digit',
+                        })
                       ) : (
                         row[column.id]
                       )}
                     </TableCell>
-                    ))}
-                    <TableCell>
+                  ))}
+                  <TableCell>
                     <Button variant="outlined" onClick={() => handleEditRecord(row)} sx={{ color: '#f56c6c', borderColor: '#f56c6c', '&:hover': { borderColor: '#f56c6c', backgroundColor: '#fbe8e8' } }} >
                       Editar
-                      </Button>
-                    </TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
             </Table>
           </TableContainer>
           <TablePagination
@@ -242,8 +330,8 @@
             onPageChange={handleChangePage}
             onRowsPerPageChange={handleChangeRowsPerPage}
           />
-           <Dialog open={openDialog} onClose={handleCloseDialog} maxWidth="md" fullWidth >
-          <DialogTitle>{editMode ? "Editar Registro" : "Agregar Nuevo Registro"}</DialogTitle>
+          <Dialog open={openDialog} onClose={handleCloseDialog} maxWidth="md" fullWidth >
+          <DialogTitle>{editMode ? "Editar Registro" : "Agregar Nuevo Equipo"}</DialogTitle>
           <form onSubmit={handleSubmit}>
             <DialogContent>
               <TextField name="nombre_equipo" label="Nombre Equipo" value={editRecord.nombre_equipo || ""} onChange={handleInputChange} fullWidth required sx={{ mb: 2 }} />
@@ -251,6 +339,7 @@
               <TextField name="tipo" label="Tipo" value={editRecord.tipo || ""} onChange={handleInputChange} fullWidth required sx={{ mb: 2 }} />
               <TextField name="marca" label="Marca" value={editRecord.marca || ""} onChange={handleInputChange} fullWidth required sx={{ mb: 2 }} />
               <TextField name="modelo" label="Modelo" value={editRecord.modelo || ""} onChange={handleInputChange} fullWidth required sx={{ mb: 2 }} />
+              
               <FormControl sx={{ mb: 2 }} fullWidth required>
                 <InputLabel id="estado-label">Estado</InputLabel>
                 <Select labelId="estado-label" id="estado-select" name="estado" value={editRecord.estado || ""} onChange={handleInputChange} label="Estado">
@@ -260,13 +349,35 @@
                 </Select>
               </FormControl>
               <TextField name="ubicación" label="Ubicación" value={editRecord.ubicación || ""} onChange={handleInputChange} fullWidth required sx={{ mb: 2 }} />
+              <TextField
+                name="descripcion"
+                label="Descripción"
+                value={editRecord.descripcion || ""}
+                onChange={handleInputChange}
+                fullWidth
+                multiline
+                rows={4} 
+                required
+                sx={{ mb: 2 }}
+              />
+              <input type="file" onChange={handleFileChange} accept="image/*" />
+              {loading && <CircularProgress sx={{ display: 'block', margin: '20px auto' }} />}
+              {imageUploaded && imageBase64 && (
+                <Typography variant="body2" color="textSecondary" sx={{ mt: 2 }}>
+                  Imagen subida: <a href={imageBase64} target="_blank" rel="noopener noreferrer">Ver Imagen</a>
+                </Typography>
+              )}
             </DialogContent>
             <DialogActions>
-              <Button onClick={handleCloseDialog} color="secondary">Cancelar</Button>
-              <Button type="submit" variant="contained" sx={{ backgroundColor: "#d01c35" }}>{editMode ? "Guardar Cambios" : "Agregar"}</Button>
+              <Box sx={{ display: "flex", justifyContent: "center", width: '100%' }}>
+                <Button onClick={handleCloseDialog} sx={{ color: '#f56c6c', borderColor: '#f56c6c', '&:hover': { borderColor: '#f56c6c', backgroundColor: '#fbe8e8' },marginRight:2 }}variant="outlined">Cancelar</Button>
+                <Button type="submit" variant="contained" sx={{ backgroundColor: "#d01c35" }}>{editMode ? "Guardar Cambios" : "Agregar"}</Button>
+              </Box>
+              
             </DialogActions>
           </form>
         </Dialog>
+        <ToastContainer />
       </Box>
     </Box>
   );
--- src/Components/DashboardUsuarios/prestamos.jsx
+++ src/Components/DashboardUsuarios/prestamos.jsx
@@ -1,14 +1,14 @@
 import React, {useState, useEffect,useRef} from 'react'
 import Navbar from './sidebarus'
-import {Button,Box,TextField,Typography,Table,TableContainer,TableHead,TableRow,TableCell,TableBody,Paper,TablePagination, TableSortLabel
-,Dialog, DialogActions, DialogContent,
-DialogTitle} from "@mui/material";
+import {Button,Box,TextField,Typography,Table,TableContainer,TableHead,TableRow,TableCell,TableBody,Paper,TablePagination, TableSortLabel,Dialog, DialogActions, DialogContent, DialogTitle, Popover} from "@mui/material";
 import axios from "axios";
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import dayjs from 'dayjs';
 import Cookies from 'js-cookie';
+import { ToastContainer, toast } from 'react-toastify'; 
+import 'react-toastify/dist/ReactToastify.css';
 
 
 
@@ -17,6 +17,7 @@ const columns = [
   { id: "tipo", label: "Tipo" },
   { id: "marca", label: "Marca" },
   { id: "modelo", label: "Modelo" },
+  { id : "descripcion", label: "Descripcion"}
 ];
 function Prestamos() {
   const [open, setOpen] = useState(false);
@@ -30,6 +31,9 @@ function Prestamos() {
   const [hoveredRow, setHoveredRow] = useState(null);
   const [reservationDate, setReservationDate] = useState(dayjs());
   const [searchTerm, setSearchTerm] = useState('');
+  const [anchorEl, setAnchorEl] = useState(null); 
+  const [popoverData, setPopoverData] = useState(null);
+
   const today = dayjs();
   const maxDate = today.add(7, 'day');
 
@@ -107,13 +111,13 @@ function Prestamos() {
         const response = await axios.post("http://localhost:3001/api/prestamos", payload);
     
         if (response.status === 201) {
-          alert('Préstamo registrado con éxito');
+          toast.success("Prestamo realizado con éxito!",{autoClose:2000});
           handleClose(); 
           fetchData();
         }
       } catch (error) {
         console.error("Error al registrar el préstamo:", error);
-        alert("Hubo un error al registrar el préstamo");
+        toast.success("Error al reservar prestamo",{autoClose:2000});
       }
     };
     const handleSearchChange = (event) => {
@@ -124,7 +128,20 @@ function Prestamos() {
       setFilteredData(filtered);
       setPage(0);
     };
-   
+    const handlePopoverOpen = (event, row) => {
+      setAnchorEl(event.currentTarget); 
+      setPopoverData(row); 
+      setHoveredRow(row.id_equipo);
+    };
+  
+    const handlePopoverClose = () => {
+      setAnchorEl(null); 
+      setPopoverData(null);
+    };
+  
+    const openPopover = Boolean(anchorEl); 
+  
+    const idPopover = openPopover ? 'simple-popover' : undefined;
   return (
     <Box sx={{}}>
       <Navbar />
@@ -139,73 +156,98 @@ function Prestamos() {
           onChange={handleSearchChange}
           sx={{ mb: 3, width: '300px' }}
         />
-        <TableContainer component={Paper} sx={{borderRadius: '10px', boxShadow: '0px 4px 20px rgba(0, 0, 0, 0.1)', width: '100%', border: '1px solid #ddd' }}>
-          <Table>
-            <TableHead>
-              <TableRow >
+        <TableContainer component={Paper} sx={{ borderRadius: '10px', boxShadow: '0px 4px 20px rgba(0, 0, 0, 0.1)', width: '100%', border: '1px solid #ddd' }}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              {columns.map((column) => (
+                <TableCell 
+                  key={column.id} 
+                  sx={{ fontWeight: 'bold', padding: '8px 10px', borderBottom: '2px solid #ddd' }}
+                >
+                  <TableSortLabel
+                    active={orderBy === column.id}
+                    direction={orderBy === column.id ? order : "asc"}
+                    onClick={() => handleSort(column.id)}
+                  >
+                    {column.label}
+                  </TableSortLabel>
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {sortedData.map((row) => (
+              <TableRow 
+                key={row.id_equipo} 
+                sx={{ '&:hover': { backgroundColor: '#e0f7fa' }, height: '50px' }} 
+                onMouseEnter={() => setHoveredRow(row.id_equipo)} onMouseLeave={() => setHoveredRow(null)}
+              >
                 {columns.map((column) => (
-                  <TableCell key={column.id} sx={{fontWeight: 'bold' , padding: '20px',borderBottom: '2px solid #ddd'}}>
-                    <TableSortLabel
-                      active={orderBy === column.id}
-                      direction={orderBy === column.id ? order : "asc"}
-                      onClick={() => handleSort(column.id)}
-                    >
-                      {column.label}
-                    </TableSortLabel>
+                  <TableCell 
+                    key={column.id} 
+                    sx={{ borderBottom: '1px solid #ddd', padding: '8px 10px' }} 
+                  >
+                    {row[column.id]}
                   </TableCell>
                 ))}
-              
+                <TableCell sx={{ textAlign: 'right', width: '150px', overflow: 'hidden', padding: '8px 10px' }}>
+                  {hoveredRow === row.id_equipo && (
+                    <Button 
+                      variant="contained" 
+                      sx={{ padding: '5px 10px', minWidth: '80px', backgroundColor: "#d01c35"}}  onClick={() => handleOpen(row)}>
+                      Reservar
+                    </Button>
+                  )}
+                </TableCell>
               </TableRow>
-            </TableHead>
-            <TableBody>
-              {sortedData.map((row) => (
-                <TableRow key={row.id_equipo} sx={{ '&:hover': { backgroundColor: '#e0f7fa' } }}  onMouseEnter={() => setHoveredRow(row.id_equipo)} onMouseLeave={() => setHoveredRow(null)} >
-                  {columns.map((column) => (
-                    <TableCell key={column.id} sx={{borderBottom: '1px solid #ddd'}}>{row[column.id]}</TableCell>
-                  ))}
-                  <TableCell sx={{ textAlign: 'right', width: '150px', overflow: 'hidden' }}>
-                    {hoveredRow === row.id_equipo && (
-                      <Button variant="contained" color="primary" sx={{ padding: '5px 10px', minWidth: '80px' }} onClick={() => handleOpen(row)}>
-                        Reservar
-                      </Button>
-                    )}
-                  </TableCell>
-                  
-
-                </TableRow>
-              ))}
-            </TableBody>
-            <Dialog  open={open} onClose={handleClose} fullWidth maxWidth="md" sx={{'& .MuiDialog-paper': {width: '20%', maxWidth: 'none', padding: '24px'}}}>
+            ))}
+          </TableBody>
+            <Dialog  open={open} onClose={handleClose} fullWidth maxWidth="md" sx={{'& .MuiDialog-paper': {width: '20%', maxWidth: 'none', padding: '24px',borderRadius: '8px'}}}>
             <DialogTitle sx={{ textAlign: 'center', fontSize: '1.5rem', fontWeight: 'bold' }}>
               Reservar equipo
             </DialogTitle>
-            <DialogContent sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
+            <DialogContent sx={{ display: 'flex', justifyContent: 'flex-start', alignItems: 'center', flexDirection: 'column',padding: '20px' }}>
               {selectedRow && (
-                <Box sx={{ width: '100%', mb: 2, textAlign: 'center' }}>
-                  <Typography variant="h6" gutterBottom>Detalles del equipo:</Typography>
-                  <Typography>Nombre: {selectedRow.nombre_equipo}</Typography>
-                  <Typography>Tipo: {selectedRow.tipo}</Typography>
-                  <Typography>Marca: {selectedRow.marca}</Typography>
-                  <Typography>Modelo: {selectedRow.modelo}</Typography>
+                <Box sx={{ width: '100%', mb: 3,display:'flex',alignItems:'flex-start',flexDirection: 'column', }}>
+                  <Typography variant="h6" gutterBottom sx={{ fontWeight: 'bold', color: '#333' }}>Detalles del equipo:</Typography>
+                  <Typography  sx={{ fontSize: '16px', marginBottom: '8px'}}><strong>Nombre:</strong> {selectedRow.nombre_equipo}</Typography>
+                  <Typography  sx={{ fontSize: '16px', marginBottom: '8px' }}><strong>Tipo:</strong>: {selectedRow.tipo}</Typography>
+                  <Typography  sx={{ fontSize: '16px', marginBottom: '8px' }}><strong>Marca:</strong> {selectedRow.marca}</Typography>
+                  <Typography  sx={{ fontSize: '16px', marginBottom: '8px' }}><strong>Modelo:</strong> {selectedRow.modelo}</Typography>
+                  <Typography  sx={{ fontSize: '16px', marginBottom: '8px' }}><strong>Descripcion:</strong> {selectedRow.descripcion}</Typography>
+                  <Typography  sx={{ fontSize: '16px', marginBottom: '8px' , fontWeight:'bold'}}>Imagen del equipo: </Typography>
+                  <Box sx={{ width: '100%', display: 'flex', justifyContent: 'center', my: 2 }}>
+                    <img
+                      src={selectedRow.imagen || 'default-image.jpg'}
+                      alt="Equipo"
+                      style={{
+                        width: '150px',
+                        height: '150px',
+                        borderRadius: '8px',
+                        boxShadow: '0 4px 10px rgba(0, 0, 0, 0.1)',
+                      }}
+                    />
+                  </Box>
+                  <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <DatePicker
+                      label="Fecha de reserva"
+                      value={reservationDate}
+                      onChange={handleDateChange}
+                      minDate={today}
+                      maxDate={maxDate}
+                      renderInput={(params) => <TextField {...params} fullWidth variant="outlined" sx={{ maxWidth: '400px' }} />}
+                    />
+                  </LocalizationProvider>
+                      
                 </Box>
               )}
-
-              <LocalizationProvider dateAdapter={AdapterDayjs}>
-                <DatePicker
-                  label="Fecha de reserva"
-                  value={reservationDate}
-                  onChange={handleDateChange}
-                  minDate={today}
-                  maxDate={maxDate}
-                  renderInput={(params) => <TextField {...params} fullWidth variant="outlined" sx={{ maxWidth: '400px' }} />}
-                />
-              </LocalizationProvider>
             </DialogContent>
             <DialogActions sx={{ justifyContent: 'center', padding: '16px' }}>
-              <Button onClick={handleClose} color="primary" variant="outlined">
+              <Button onClick={handleClose}  sx={{ color: '#f56c6c', borderColor: '#f56c6c', '&:hover': { borderColor: '#f56c6c', backgroundColor: '#fbe8e8' }}} variant="outlined">
                 Cancelar
               </Button>
-              <Button onClick={handleConfirmar} color="primary" variant="contained">
+              <Button onClick={handleConfirmar} sx={{ backgroundColor: "#d01c35" }} variant="contained">
                 Confirmar
               </Button>
             </DialogActions>
@@ -222,6 +264,33 @@ function Prestamos() {
           onRowsPerPageChange={handleChangeRowsPerPage}
         />
       </Box>
+      <Popover
+        id={idPopover}
+        open={openPopover}
+        anchorEl={anchorEl}
+        onClose={handlePopoverClose}
+        anchorOrigin={{
+          vertical: 'top', 
+        }}
+        transformOrigin={{
+          vertical: 'bottom', 
+          horizontal: 'center',
+        }}
+      >
+        {popoverData && (
+          <Box sx={{ p: 2 }}>
+            <Typography variant="h6">Detalles del equipo</Typography>
+            <Typography>Nombre: {popoverData.nombre_equipo}</Typography>
+            <img 
+              src={popoverData.imagen || 'default-image.jpg'} 
+              alt="Equipo" 
+              style={{ width: '100px', height: 'auto' }} 
+            />
+            <Typography>{popoverData.descripcion}</Typography>
+          </Box>
+        )}
+      </Popover>
+      <ToastContainer />
     </Box>
   );
 }
