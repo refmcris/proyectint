916537a154795246ba102eb36ef4af10d38c013b
refmcris <cristianmartineztez@gmail.com>
2024-12-03 23:27:55 -0500

    login try

--- src/Auth/Login.jsx
+++ src/Auth/Login.jsx
@@ -31,7 +31,7 @@ const Login = () => {
   };
   const handleLogin = async () => {
     try {
-      const response = await axios.post('http://localhost:5000/api/login', {
+      const response = await axios.post('https://uniback.onrender.com/api/login', {
         email: emailInput,
         password: passwordInput
       });
